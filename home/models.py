0305cfb35be4fcaf86bca408438b78585a9b2c67
Dhiraj kumar <dhiraj.kumar@nescode.com>
2019-12-15 21:27:54 +0530

    project modified

--- home/models.py
+++ home/models.py
@@ -2,14 +2,33 @@ from django.db import models
 
 from wagtail.core.models import Page
 from wagtail.core.fields import RichTextField
-from wagtail.admin.edit_handlers import FieldPanel
+from wagtail.images.edit_handlers import ImageChooserPanel
+from wagtail.admin.edit_handlers import FieldPanel, MultiFieldPanel, InlinePanel
 
 
 class HomePage(Page):
     head = RichTextField(blank=True)
     body = RichTextField(blank=True)
+    caption1 = RichTextField(blank=True)
+    caption2 = RichTextField(blank=True)
+    caption3 = RichTextField(blank=True)
+    panels=[
+        FieldPanel('caption1'),
+        FieldPanel('caption2'),
+        FieldPanel('caption3'),
+        ]
+
+    icon = models.ForeignKey(
+        'wagtailimages.Image', null=True, blank=True,
+        on_delete=models.SET_NULL, related_name='+'
+    )
 
     content_panels = Page.content_panels + [
         FieldPanel('head', classname="full"),
         FieldPanel('body', classname="full"),
+        FieldPanel('caption1'),
+        FieldPanel('caption2'),
+        FieldPanel('caption3'),
+        ImageChooserPanel('icon'),
     ]
+   
--- shopapp/models.py
+++ shopapp/models.py
@@ -1,11 +1,62 @@
-from wagtail.core.models import Page
+from django.db import models
+
+from wagtail.core.models import Page, Orderable
 from wagtail.core.fields import RichTextField
 from wagtail.admin.edit_handlers import FieldPanel
-
+from wagtail.search import index
+from taggit.models import TaggedItemBase
+from modelcluster.fields import ParentalKey
+from wagtail.admin.edit_handlers import FieldPanel, InlinePanel, MultiFieldPanel
+from modelcluster.contrib.taggit import ClusterTaggableManager
+from wagtail.images.edit_handlers import ImageChooserPanel
 
 class ShopIndexPage(Page):
     intro = RichTextField(blank=True)
 
     content_panels = Page.content_panels + [
         FieldPanel('intro', classname="full")
+    ]
+
+
+
+class ShopPageTag(TaggedItemBase):
+    content_object = ParentalKey(
+        'ShopPage',
+        related_name='tagged_items',
+        on_delete=models.CASCADE
+    )
+
+
+
+class ShopPage(Page):
+    date = models.DateField("Post date")
+    intro = models.CharField(max_length=250)
+    body = RichTextField(blank=True)
+    tags = ClusterTaggableManager(through=ShopPageTag, blank=True)
+
+    search_fields = Page.search_fields + [
+        index.SearchField('intro'),
+        index.SearchField('body'),
+    ]
+
+    content_panels = Page.content_panels + [
+        MultiFieldPanel([
+            FieldPanel('date'),
+            FieldPanel('tags'),
+        ], heading="Shop information"),
+        FieldPanel('intro'),
+        FieldPanel('body', classname="full"),
+        InlinePanel('gallery_images', label="Gallery images"),
+    ]
+
+class ShopPageGalleryImage(Orderable):
+    page = ParentalKey(ShopPage, on_delete=models.CASCADE, related_name='gallery_images')
+    image = models.ForeignKey(
+        'wagtailimages.Image', on_delete=models.CASCADE, related_name='+'
+    )
+    caption = models.CharField(blank=True, max_length=250)
+
+    panels = [
+        ImageChooserPanel('image'),
+        FieldPanel('caption'),
     ]
